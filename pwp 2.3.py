fc4730f0a524f219541733eb4586fdb59682f072
karthikkumargoud <chittimodu.kumar129396@marwadiuniversity.ac.in>
2025-07-22 17:22:56 +0530

    Add files via upload

---
+++ pwp 2.3.py
@@ -0,0 +1,7 @@
+side = float(input("Enter the side length of the cube: "))
+
+area = 6 * side ** 2
+volume = side ** 3
+
+print(f"Surface Area: {area}")
+print(f"Volume: {volume}")
---
+++ pwp 2.4.py
@@ -0,0 +1,7 @@
+x = float(input("Enter the value of x: "))
+y = float(input("Enter the value of y: "))
+
+z = (x + y) * (x - y)
+
+print(f"z = {z}")
+
