11a70028f2feb50eaa838a123fb817fe0bb1a4e5
karthikkumargoud <chittimodu.kumar129396@marwadiuniversity.ac.in>
2025-09-27 21:57:35 +0530

    Add files via upload

---
+++ 17.1.py
@@ -0,0 +1,11 @@
+import sympy as sp
+
+n, z = sp.symbols('n z')
+
+# Define the sequence x[n] = 3^n * u[n]
+x_n = 3**n
+
+X_z = sp.summation(x_n * z**(-n), (n, 0, sp.oo))
+
+print("Z-transform of x[n] = 3^n u[n]:")
+sp.pprint(X_z, use_unicode=True)
